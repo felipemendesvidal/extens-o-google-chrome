69fb376012a18d9b9e90fc9b181246362350519c
felipe <contatofelipevidalmendes@gmail.com>
2023-02-01 14:57:58 -0300

    versão simples funcionando

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.contextMenus.create({
     title: "Tirar Screenshot",
     contexts: ["all"],
     onclick: function() {
-        
+
       // captura a tab ativa
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var tab = tabs[0];
@@ -11,5 +11,16 @@ chrome.contextMenus.create({
         // Send a message to the content script to capture the screenshot
         chrome.tabs.sendMessage(tab.id, { action: "startSelection" });
       });
+    }
+  });
+  
+  // listem para as Escuta as mensagens do script 
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "saveScreenshot") {
+      // Get the screenshot data
+      var data = request.data;
+  
+      // Create a new tab with the screenshot data
+      chrome.tabs.create({ url: data });
     }
   });
---
+++ content.js
@@ -0,0 +1,8 @@
+chrome.tabs.captureVisibleTab(null, {}, function(image) {
+      var link = document.createElement("a");
+      link.download = "screenshot.png";
+      link.href = image;
+      link.click();
+    });
+
+
